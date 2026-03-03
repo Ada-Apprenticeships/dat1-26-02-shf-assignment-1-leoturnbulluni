cca0ab7770d8a1d47cbfa3f351303cb03150bb7c
leoturnbulluni <leo.turnbull@ada.ac.uk>
2026-03-03 23:21:10 +0000

    final update

--- src/queries/8_personal_training.sql
+++ src/queries/8_personal_training.sql
@@ -2,4 +2,12 @@
 .mode column
 
 -- 8.1 
-
+SELECT pts.session_id,
+	m.first_name || ' ' || m.last_name AS member_name, -- combine member first and last name, with a blank space between
+	pts.session_date,
+	pts.start_time,
+	pts.end_time
+FROM personal_training_sessions pts
+JOIN staff s ON s.staff_id = pts.staff_id --        combine with staff table (to find trainer name)
+JOIN members m ON m.member_id = pts.member_id --    combine with members table (to access member names)
+WHERE s.first_name = 'Ivy' AND s.last_name = 'Irwin';
